6c1c0b51ea97990b67affc12e4352634deb36476
sirmax <samuelbernal310@gmail.com>
2025-07-14 23:22:51 -0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const photoContainer = document.getElementById('photo-container');
 
-photoContainer.innerHTML = getPhotoElement(1);
+let content = `<div class="column">`;
+
+
+for(let i=0; i<20; i++){
+
+    if(i!=0 && (i % 5 ==0)){
+       content += `</div><div class="column">
+       ${getPhotoElement(i+1)}`;
+    }else{
+        content += getPhotoElement(i+1);
+    }
+}
+content += `</div>`;
+
+photoContainer.innerHTML += content;
 
 function getPhotoElement(photo_id){
     return `<a href="#" class="photo">
